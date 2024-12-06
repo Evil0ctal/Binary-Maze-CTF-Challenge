978cab9dae4b2007d31d413a0993e3cb2ee7bca0
Evil0ctal <evil0ctal1985@gmail.com>
2024-12-06 01:13:40 -0800

    Update source.py

--- source.py
+++ source.py
@@ -1,37 +1,89 @@
 # Th3_d33p3r_y0u_l00k_th3_d4rk3r_1t_g3ts...
 # s0m3t1m3s_th3_truth_1s_h1dd3n_1n_pl41n_s1ght
+
+import mmap as _______;
+import os;
+import random as _____;
+import signal as _________;
+import threading as ________;
+import time as ______;
+
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
 # 01101101 01100001 01111001 01100010 01100101
-(lambda τ,π,μ,ρ,σ:exec(τ(π(μ(''.join([chr(ord(c)^42)for c in"¦´±¤¨¶·²¥­¶¶®¯¥¤´±¤¶·¦²¨±·§´±¦¨¸±¦·¦´±¨¥¥²¶·§´±¸·²µ¦¶°¦¶£¬®¬¨¶¸¦µ"])))))))(eval,''.join,list,chr,ord)
-ζ=type('ζ',(object,),{'ζ':lambda υ:eval(''.join(chr(ord(c)^ord(k))for c,k in zip(υ,'key'*len(υ))))})().ζ
-def θ(n):return''.join(chr(((ord(c)-32+n)%95)+32)for c in ζ(''.join(chr(ord(c)^0x55)for c in n)))
-class Ξ(type):
- @staticmethod
- def __new__(λ,*args,**kwargs):return super().__new__(λ,θ('κλμ'),(),{})
-class Δ(metaclass=Ξ):pass
-# wh3r3_d03s_th3_c0d3_r34lly_st4rt?
-____=eval('chr');exec(____((ord('O')+ord('P'))//2)+'__import__("base64")'+'as'+'_')
-δ=_;α=μ;ε=σ;η=ρ;γ=π;β=τ
-# th3_k3y_1s_n0t_wh4t_y0u_th1nk
-exec(_.b64decode(''.join(chr(ord(c)^0x17)for c in"""gSwXPRU+EhwKBhQEFQMWBxIDEgIQBBcBEQwVEAMWChQAEQERAhAKFwgSBhULERgWBBcHEgQUBBMBGwYSCxcHEgQXAxAFFwgSABIAEg4WBBEKFAgWDhQGEQEUBxABFQwSBBcBER4QARUAEQQSABUBFxEQBRQJEhwWDhcIEgYWBRIHFwEUABELEgUUABMAFwgUBBEBEQoTCRUMEgQXChIEFwYSAhEBFA="""))
-# 3v3ry_p4th_l34ds_t0_th3_s4m3_pl4c3
-exec(''.join([chr(ord(x)^42)for x in"""ÈÕÒØÃÂÊØÌÊÉØÒÊÕÊØÖÒÕÍÑÌÖØÃÕÊØÑÔØÐÔÕÊØÒÎÃÑØÏÙÖÒØÖÒÕÍÑÌÖ¿¿¿ØÒÎÊÞØÐÍÌÎÒØÇÊØÒÎÊØÓÃÒÎ"""]))
-#n0t3: th3_fl4g_1s_cl0s3r_th4n_y0u_th1nk
-class _____:
- def __λ__(μ):μ.π=[0]*16;μ.ω=__import__('mmap').mmap(-1,4096);μ.ψ=0;μ.φ=[];μ.χ={};μ.θ=[];μ.Ω=3;μ.Φ=__import__('threading').Thread(target=μ.Ψ);μ.Φ.daemon=True;μ.Φ.start();__import__('signal').signal(2,μ.Σ)
- def Ψ(μ):
-  while 1:
-   if μ.Γ():μ.Θ()
-   __import__('time').sleep(__import__('random').random())
- def Σ(μ,Λ,Δ):μ.π=[__import__('random').randint(0,0xFFFFFFFF)for _ in range(16)]
- # m4yb3_th3_fl4g_1s_h1dd3n_1n_th3_c0d3
-exec(_.b64decode(''.join([chr(ord(c)^0x33)for c in"""ZktGTktITkVLRE5CS0ZORUpLR05FTEtHTk1LQk5PS0ROREtFTkZLSE5IS0JOQktETk1LRk5FTEdOTEtCTkpLRE5NS0ZOSEtHTkJLRE5FS0dOTUtGTkVMR05MS0JOSktETk1LRk5IS0dOQktETkVLR05NS0ZORUxHTkxLQk5KS0ROTUtGTkhLR05CS0RORUtHTk1LRk5FTEdOTEtCTkpLRE5NS0ZOSEtHTkJLRE5FS0dOTUtGTkVMR04="""])))
-# th3_4nsw3r_1s_1n_th3_qu3st10n
-def ________():
- if _____.__λ__():print(θ("Nice try! :)"));return
- ν=_____();ο=______();ι=_________;κ=__________;ς=b"".join([bytes([x^42])for x in[94,108,97,103,123,77,97,115,55,101,114,95,48,102,95,66,105,110,52,114,121,95,67,104,52,111,115,95,50,48,50,52,125]]);π=________(ς);χ=ν.γ(π);ω=ι.γ(χ);ε,ι=κ.ε(ω)
- with open(θ("binary_maze"),"wb")as f:f.write(ε)
- with open(θ("binary_maze.iv"),"wb")as f:f.write(ι)
-#cr3d1ts: th3_cr34t0r_0f_th1s_puzzl3_h4s_l3ft_s0m3_tr4c3s
-if __name__=="__main__":________()
-# f1n4l_h1nt: wh4t_y0u_s33_m1ght_b3_4_l13
-# r3m3mb3r: s0m3t1m3s_th3_s1mpl3st_p4th_1s_th3_h4rd3st_t0_f1nd
+# 01110100 01101000 01100101 00100000 01110010
+# 01100101 01110011 01110100 00100000 01101001
+# 01110011 00100000 01101000 01100101 01110010 01100101
+
+λ = type('λ', (), {'δ': lambda x: eval(x)})()
+Ω = lambda x: x if isinstance(x, str) else ''.join(chr(ord(c)) for c in str(x))
+Σ = lambda x: bytes([ord(c) ^ 42 for c in x])
+
+
+class Δ:
+    def __init__(self):
+        self.μ = [0] * 16
+        self.π = _______.mmap(-1, 4096)
+        self.θ = 0
+        self.ξ = []
+        self.ψ = {}
+        self.φ = []
+        self.ω = 3
+        self.γ = ________.Thread(target=self._α)
+        self.γ.daemon = True
+        self.γ.start()
+        _________.signal(2, self._β)
+
+    def _α(self):
+        while True:
+            if self._δ():
+                self._ε()
+            ______.sleep(_____.random())
+
+    def _β(self, σ, τ):
+        self.μ = [_____.randint(0, 0xFFFFFFFF) for _ in range(16)]
+
+    def _δ(self):
+        try:
+            return ______.time() - ______.time() > 0.1
+        except:
+            return True
+
+    def _ε(self):
+        self.μ = [_____.randint(0, 255) for _ in range(16)]
+
+
+def η():
+    if Δ()._δ():
+        print("Nice try! :)")
+        return
+
+    # Original flag string (don't look here!)
+    ζ = "flag{M4st3r_0f_B1n4ry_Ch40s_2024}"
+
+    # Multiple layers of transformations
+    κ = [ord(c) for c in ζ]
+    ι = [x ^ 42 for x in κ]
+    θ = [x ^ 0x55 for x in ι]
+    ρ = bytes([x ^ 0x33 for x in θ])
+
+    # Additional encryption layers
+    σ = os.urandom(32)
+    τ = AESGCM(σ)
+    υ = os.urandom(12)
+    φ = τ.encrypt(υ, ρ, None)
+
+    with open("binary_maze", "wb") as f:
+        f.write(φ)
+    with open("binary_maze.iv", "wb") as f:
+        f.write(υ)
+
+
+# cr3d1ts: th3_cr34t0r_0f_th1s_puzzl3_h4s_l3ft_s0m3_tr4c3s
+if __name__ == "__main__":
+    η()
+
+# f1n4l_h1nt: l00k_cl0s3ly_4t_th3_tr4nsf0rm4t10ns
+# r3m3mb3r: th3_k3y_t0_und3rst4nd1ng_1s_1n_th3_p4tt3rns
+#
+# ...01011111 01100101 01101110 01100100...
